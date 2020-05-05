4c1dccd951f133d17da9f3a14710e3e60b34d516
James Ha <jameshahaha@icloud.com>
2020-05-05 00:54:03 -0600

    added logo

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { TopNav } from './TopNav/TopNav';
+import logo from '../assets/critterpedia-full.png';
+import styles from './LandingPage.module.css';
 
 export function LandingPage(){
     return(
         <div>
             <TopNav/>
+            <img src={logo} className={styles.logo} alt="logo"/>
         </div>
     );
 }
