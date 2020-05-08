156ff2a2726a7105eaeab5cf67648e9f04e82dad
James Ha <jameshahaha@icloud.com>
2020-05-08 14:17:40 -0600

    component alignment

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -7,11 +7,13 @@ import styles from './LandingPage.module.css';
 
 export function LandingPage(){
     return(
-        <div>
-            <TopNav/>
-            <img src={logo} className={styles.logo} alt="logo"/>
-            <SearchBar/>
-            <SearchSuggestions/>
+        <div className={styles.landing}>
+            <div className={styles['search-area']}>
+                <TopNav/>
+                <img src={logo} className={styles.logo} alt="logo"/>
+                <SearchSuggestions/>
+                <SearchBar/>
+            </div>
         </div>
     );
 }
